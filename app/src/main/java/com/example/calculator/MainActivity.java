e52c01b81dc3ff0c6ff461d6f07739429cdd8b7c
bibektamang873 <bibektamang873@gmail.com>
2019-10-20 16:19:33 +0545

    commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnAdd,
+               btnSubtract, btnMultiply, btnDivide, btnClear, btnEquals;
+    EditText editTextRs;
+    String displaynum;
+    int num1, num2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btn0 = findViewById(R.id.btn0);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSubtract = findViewById(R.id.btnSubtract);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnClear = findViewById(R.id.btnClear);
+        btnEquals = findViewById(R.id.btnEquals);
+        editTextRs = findViewById(R.id.editTextRs);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
